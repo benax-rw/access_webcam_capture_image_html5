84d976e5e3224de1f2f7b9bb2f75ad8a95e13b74
unknown <rahulkrdaman@gmail.com>
2020-10-25 10:43:30 +0530

    webcam

---
+++ ReadMe.txt
@@ -0,0 +1,5 @@
+Author: Rahul Kr Daman
+
+Instructions:
+==================================================
+Open the index.html file on the browser and allow to access your webcam. You will see the webcam streaming on the webpage. Click on the "Capture" button to take picture from the webcam.
---
+++ js/webcam.js
@@ -0,0 +1,46 @@
+/*
+*  Copyright (c) 2020 CodexWorld. All Rights Reserved.
+*
+*  @url https://www.codexworld.com/
+*/
+
+'use strict';
+
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const snap = document.getElementById("snap");
+const errorMsgElement = document.querySelector('span#errorMsg');
+
+const constraints = {
+  audio: true,
+  video: {
+    width: 1280, height: 720
+  }
+};
+
+// Access webcam
+async function init() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleSuccess(stream);
+  } catch (e) {
+    console.error('navigator.getUserMedia error:', e);
+    errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
+  }
+}
+
+// Success
+function handleSuccess(stream) {
+  console.log('getUserMedia() got stream:', stream);
+  window.stream = stream;
+  video.srcObject = stream;
+}
+
+// Load init
+init();
+
+// Draw image
+var context = canvas.getContext('2d');
+snap.addEventListener("click", function() {
+	context.drawImage(video, 0, 0, 640, 480);
+});
